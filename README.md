a017547af6ca11a616726a96891f34ecf967d40c
Yash <79582883+yash-aryan@users.noreply.github.com>
2022-12-06 17:34:07 +0530

    Add javascript logic to play multiple round
    Finished the project by adding all the required script logic to play
    5 rounds of RPS. In the end it displays the score & declares the winner.
    I also updated the .html file with instructions, and updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rock-paper-scissor
-This is a Javascript focused learning project while I was doing Odin Project Foundations
+This is a Javascript focused learning project while I was doing Odin Project Foundations. It's a rock-paper-scissor game upto 5 rounds against a computer.
 
-Currently there is no GUI, the game is played using the browser pop-up menu. I will update this project soon, once I start learning even more.
+Currently there is no GUI, the game is played using the browser console & pop-up menu. I will update this project soon, once I start learning even more.
--- scripts.js
+++ scripts.js
@@ -2,50 +2,84 @@
 // Using Math.random() to generate numbers from 0 - 100, it outputs the following: 
 // "rock" for numbers  0 - 33, "paper" for 34 - 66, "scissor" for 67 - 100
 function getComputerChoice() {
-    let keyValue = Math.floor(Math.random() * 100);
+    let keyValue = 0;
+    keyValue = Math.floor(Math.random() * 100);
     switch (true) {
         case (keyValue <= 33):
-            return "rock";
+            return "ROCK";
         case (keyValue > 33 && keyValue <= 66):
-            return "paper";
+            return "PAPER";
         case (keyValue > 66):
-            return "scissor";
+            return "SCISSOR";
         default:
-            return "Something went wrong!";
+            return "Something went wrong while computer is choosing move";
     }
 }
 
 // This function just prompts an input from the user
 function getPlayerSelection() {
-    return prompt("Let's begin! Choose: Rock, Paper, or Scissor?");
+    return prompt("Choose: Rock, Paper, or Scissor?");
 }
 
 // This function plays 1 round, and outputs the result
 function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = "";
+    playerChoice = getPlayerSelection();
+    let myChoice = playerChoice.toUpperCase();
+
+    console.log(`You: ${myChoice}`);
+    console.log(`Computer: ${computerChoice}`);
+
     if (myChoice === computerChoice) {
-        return `It's a Tie! ${myChoice.toCase()} vs ${computerChoice}`;
+        console.log(`Your Score: ${playerWins}`);
+        console.log(`Computer's Score: ${computerWins}`);
+        return `It's a Tie! ${myChoice} vs ${computerChoice}`;
     }
     // All player WIN conditions mentioned in one "else if" statement
-    else if (myChoice === "rock" && computerChoice === "scissor"
-    || myChoice === "paper" && computerChoice === "rock" 
-    || myChoice === "scissor" && computerChoice === "paper") {
-        return `You Win! your ${myChoice} beats ${computerChoice}`
+    else if (myChoice === "ROCK" && computerChoice === "SCISSOR" || myChoice === "PAPER" && computerChoice === "ROCK" || myChoice === "SCISSOR" && computerChoice === "PAPER") {
+        playerWins = ++playerWins;
+        console.log(`Your Score: ${playerWins}`);
+        console.log(`Computer's Score: ${computerWins}`);
+        return `Round won! your ${myChoice} beats ${computerChoice}`;
     }
     // All player LOSE conditions mentioned in one "else if" statement
-    else if (computerChoice === "rock" && myChoice === "scissor" 
-    || computerChoice === "paper" && myChoice === "rock" 
-    || computerChoice === "scissor" && myChoice === "paper") {
-        return `You Lose! ${computerChoice} beats your ${myChoice}`
+    else if (computerChoice === "ROCK" && myChoice === "SCISSOR" || computerChoice === "PAPER" && myChoice === "ROCK" || computerChoice === "SCISSOR" && myChoice === "PAPER") {
+        computerWins = ++computerWins;
+        console.log(`Your Score: ${playerWins}`);
+        console.log(`Computer's Score: ${computerWins}`);
+        return `Round lost! ${computerChoice} beats your ${myChoice}`;
     }
     else {
-        return "Something went wrong while deciding the winner..."
+        return `"${playerChoice}" is not a valid input`;
     }
 }
 
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerSelection();
-let myChoice = playerChoice.toLowerCase();
+function game() {
+    for (let i = 1; i < 6; i++) {
+        console.log("")
+        console.log(`----------- ROUND  ${i} -----------`);
+        console.log(playRound());
+    }
+   let totalCompterWins = computerWins;
+   let totalPlayerWins = playerWins;
+
+   if (totalCompterWins > totalPlayerWins) {
+    console.log(`YOU LOSE THE GAME! computer wins: ${totalCompterWins}   your wins: ${totalPlayerWins}`);
+   }
+   else if (totalCompterWins < totalPlayerWins) {
+    console.log(`YOU WIN THE GAME!  computer wins: ${totalCompterWins}   your wins: ${totalPlayerWins}`);
+   }
+   else {
+    console.log(`GAME IS TIED!  computer wins: ${totalCompterWins}   your wins: ${totalPlayerWins}`);
+   }
+}
+
+
+let playerWins = 0;
+let computerWins = 0;
+
+game();
 
 // The resulting output
-alert(`Computer chose ${computerChoice}, which means...`);
-alert(playRound());
+// alert(`Computer chose ${computerChoice}, which means...`);
